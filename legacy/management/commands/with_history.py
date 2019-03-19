dfc06803fab18716114c82531d7e8745ca6bf381
George Hickman <george@ghickman.co.uk>
2019-03-19 13:22:16 +0000

    Don't use Matcher in with_history import command
    The PASMatcher class hierarchy added unrequired obscurity to this import
    process.

--- legacy/management/commands/with_history.py
+++ legacy/management/commands/with_history.py
@@ -2,92 +2,77 @@
 Management command to import all data to the database
 """
 import csv
-import datetime
+from datetime import date, datetime
 
 from django.core.management import BaseCommand
 from django.utils import timezone
-
-from plugins.trade.match import FieldConverter, Matcher, Mapping
+from opal.models import Patient
 
 from legacy.management.commands.flush_database import flush
 from rbhl.episode_categories import OccupationalLungDiseaseEpisode
+from rbhl.models import Demographics
+
+sexLUT = {"F": "Female", "M": "Male", "U": "Not Known"}
 
 
-def sex_to_sex(raw_data, *args, **kwargs):
-    raw = raw_data['Sex']
-    if raw == 'M':
-        return 'Male'
-    if raw == 'F':
-        return 'Female'
-    if raw == 'U':
-        return 'Not Known'
-    if raw == '':
-        return None
-    raise ValueError('Unknown Sex: {}'.format(raw))
-
-
-def date_to_dob(raw_data, *args, **kwargs):
-    raw = raw_data['Dateofbirth']
-    if raw == '':
-        return None
-    when = timezone.make_aware(
-        datetime.datetime.strptime(raw, "%d-%b-%y")
-    ).date()
-    if when > datetime.date.today():
-        when = datetime.date(when.year - 100, when.month, when.day)
-    return when
-
-
-def nhs_number_without_spaces(raw_data, *args, **kwargs):
-    number = raw_data['NHSnumber']
-    parts = number.split(' ')
-    return ''.join(parts)
-
-
-class PASMatcher(Matcher):
-    direct_match_field = 'hospital_number'
-    demographics_fields = [
-        Mapping('Hospital Number', 'hospital_number'),
-        FieldConverter(nhs_number_without_spaces, 'nhs_number'),
-        Mapping('Surname', 'surname'),
-        Mapping('Firstname', 'first_name'),
-        Mapping('Postcode', 'post_code'),
-        FieldConverter(sex_to_sex, 'sex'),
-        FieldConverter(date_to_dob, 'date_of_birth')
-    ]
-
-
-def load_PAS_demographics(file_name):
+def date_to_dob(s):
     """
-    Load the demographics from the database that talks to the PAS.
-    Use these as our basis for matching against.
+    Convert a string representing a DoB into a timezone-aware Datetime object
     """
-    patients_imported = 0
-    print('Beginning PAS DB demographics import ')
-    with open(file_name) as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            if row['Dateofbirth'] == '00-Jan-00':
-                continue
-            matcher = PASMatcher(row)
-            patients_imported += 1
-            patient = matcher.create()
-            patient.create_episode(
-                category_name=OccupationalLungDiseaseEpisode.display_name
-            )
-            if patients_imported % 500 == 0:
-                print(patients_imported)
-    print('Imported {} patients'.format(patients_imported))
+    if s == "":
+        return None
+
+    when = timezone.make_aware(datetime.strptime(s, "%d-%b-%y")).date()
+
+    if when <= date.today():
+        return when
+
+    # Handle edge case of DoB being in the future
+    return date(when.year - 100, when.month, when.day)
 
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
-        parser.add_argument(
-            'file_name',
-            help="Specify import file",
-        )
+        parser.add_argument("file_name", help="Specify import file")
 
-    def handle(self, *args, **kwargs):
-        file_name = kwargs.get("file_name")
+    def handle(self, *args, **options):
+        """
+        Load the demographics from the database that talks to the PAS.
+        Use these as our basis for matching against.
+        """
         flush()
-        load_PAS_demographics(file_name)
+
+        # Open with utf-8-sig encoding to avoid having a BOM in the first
+        # header string.
+        with open(options["file_name"], encoding="utf-8-sig") as f:
+            rows = list(csv.DictReader(f))
+
+        # ignore rows with no DoB
+        rows = filter(lambda r: r["Dateofbirth"] != "00-Jan-00", rows)
+
+        demographics = []
+        patients_imported = 0
+        for row in rows:
+            patient = Patient.objects.create()
+            patient.create_episode(
+                category_name=OccupationalLungDiseaseEpisode.display_name
+            )
+
+            demographics.append(
+                Demographics(
+                    hospital_number=row["Hospital Number"],
+                    nhs_number=row["NHSnumber"].replace(" ", ""),
+                    surname=row["Surname"],
+                    first_name=row["Firstname"],
+                    post_code=row["Postcode"],
+                    sex=sexLUT.get(row["Sex"], None),
+                    date_of_birth=date_to_dob(row["Dateofbirth"]),
+                    patient=patient,
+                )
+            )
+
+            patients_imported += 1
+
+        Demographics.objects.bulk_create(demographics)
+
+        print("Imported {} patients".format(patients_imported))
