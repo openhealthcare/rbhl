4bdd65c5803353ba01f5000dc82c2296aedf5337
David Miller <david@deadpansincerity.com>
2019-08-02 14:58:18 +0100

    Use Dict Key Instead of queryset value

--- rbhl/views.py
+++ rbhl/views.py
@@ -97,7 +97,7 @@ class ActivePatientList(ListView):
             "days_since_first_attended": "cliniclog__clinic_date",
             "seen_by": "cliniclog__seen_by"
         }
-        order_param = self.request.GET.get("order", "cliniclog__clinic_date")
+        order_param = self.request.GET.get("order", "days_since_first_attended")
 
         if order_param.startswith("-"):
             order_param = order_param[1:]
