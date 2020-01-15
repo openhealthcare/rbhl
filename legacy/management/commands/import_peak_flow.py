e36fe753bb356dd250beff8e44154f3eb29707ab
fredkingham <fredkingham@gmail.com>
2020-01-15 14:34:08 +0000

    WIP to bring in all rows from a data cut of the peak flow database

--- legacy/management/commands/import_peak_flow.py
+++ legacy/management/commands/import_peak_flow.py
@@ -3,12 +3,16 @@ Management command to bring in historic Peak Flow data
 """
 import os
 import csv
+import datetime
+from collections import defaultdict
 from django.core.management.base import BaseCommand
+from django.db import transaction
 from plugins.trade import match
 from plugins.trade.exceptions import PatientNotFoundError
 
 from rbhl.models import PeakFlowDay
-from legacy.models import PeakFlowIdentifier
+from legacy.models import PeakFlowIdentifier, OccTrial, OccTrialDay
+from opal.models import Patient
 
 
 class Matcher(match.Matcher):
@@ -23,35 +27,37 @@ class Command(BaseCommand):
             help="Specify import directory",
         )
 
-    def handle(self, *args, **kwargs):
-        print('Deleting all existing Peak Flow objects')
-        PeakFlowDay.objects.all().delete()
-        PeakFlowIdentifier.objects.all().delete()
-
-        dir_name = kwargs.get("directory_name")
-        demographics_file_name = os.path.join(dir_name, "demographics.csv")
-        flow_data_file_name = os.path.join(dir_name, "trial_day.csv")
-
-        if not os.path.exists(demographics_file_name):
-            raise ValueError(
-                "We expect a demographics file called demographics.csv"
-            )
-
-        if not os.path.exists(flow_data_file_name):
-            raise ValueError(
-                "We expect a demographics file called trial_day.csv"
-            )
-
-        self.patients_imported = 0
-        self.flow_days_imported = 0
-
-        self.patients_missed = 0
-        self.no_hosp_num = 0
-
+    def import_demographics_file(self, demographics_file_name):
         print('Import Peak Flow IDs')
-        with open(demographics_file_name) as f:
-            reader = csv.DictReader(f)
+        PeakFlowIdentifier.objects.all().delete()
+        with open(demographics_file_name, mode='r', encoding='utf-8-sig') as f:
+            reader = csv.DictReader()
             for row in reader:
+                pfi = PeakFlowIdentifier()
+
+                if not row["CRN"] == 'NULL':
+                    pfi.hospital_number = row["CRN"]
+
+                row["occmedno"] = int(row["OCCMEDNO"])
+
+                row_height = int(row["HEIGHT"])
+
+                if not row_height == 0:
+                    pfi.height = row_height
+
+                if row["SEX"] == "M":
+                    pfi.sex = pfi.MALE
+                elif row["SEX"] == "F":
+                    pfi.sex = pfi.FEMALE
+
+                row_employ = row["COMPANY_NAME"].strip()
+
+                if not row_employ == "0":
+                    pfi.company = row_employ
+
+                pfi.name = row["PAT_NAME"]
+
+
 
                 if row["CRN"] == 'NULL':
                     self.no_hosp_num += 1
@@ -64,7 +70,6 @@ class Command(BaseCommand):
                     self.patients_missed += 1
                     continue
 
-                print("Updating demographics")
                 demographics = patient.demographics()
                 demographics_changed = False
 
@@ -82,36 +87,80 @@ class Command(BaseCommand):
                 if demographics_changed:
                     demographics.save()
 
-                print('Creating Peak Flow Identifier')
                 identifier = PeakFlowIdentifier(patient=patient)
-                identifier.occmendo = int(row["OCCMEDNO"])
+                identifier.occmedno = int(row["OCCMEDNO"])
                 identifier.save()
 
-        print('Imported {} matched Peak Flow Identifiers'.format(
-            PeakFlowIdentifier.objects.all().count())
-        )
+                # at the moment no patients have more than one episode
+                episode = patient.episode_set.get()
+                employment = episode.employment_set.get()
 
-        print('Import peak flow measurements')
-        with open(flow_data_file_name) as f:
+                row_employ = row["COMPANY_NAME"].strip()
+
+                if row_employ == "0":
+                    row_employ = None
+
+                # If the don't already have an employer but there is one in the
+                # file then use that
+                if row_employ and not employment.employer:
+                    employment.employer = row_employ
+                    employment.save()
+
+            print('Imported {} matched Peak Flow Identifiers'.format(
+                PeakFlowIdentifier.objects.all().count())
+            )
+
+    def import_occ_trial_file(self, occ_trial_file_name):
+        print(f'Import OCC Trial {occ_trial_file_name}')
+        OccTrial.objects.all().delete()
+        with open(occ_trial_file_name, mode='r', encoding='utf-8-sig') as f:
             reader = csv.DictReader(f)
-
             for row in reader:
-                patients = PeakFlowIdentifier.objects.filter(
-                    occmendo=int(row["OCCMEDNO"])
-                )
-                if patients.count() > 1:
-                    print(row)
-                    raise ValueError('Too many identifiers')
-                if patients.count() == 0:
-                    print('Missed identifier - skipping')
+                model_args = {
+                    k.lower(): int(v) for k, v in row.items()
+                    if not k == "TrialID" and not k == "START_DATE"
+                }
+                model_args["trial_id"] = row["TrialID"]
+                if row["START_DATE"].strip() == "NULL":
+                    self.no_start_date += 1
                     continue
 
-                print('Creating Peak Flow Days')
-                patient = patients[0].patient
+                if "-" in row["START_DATE"]:
+                    model_args["start_date"] = datetime.datetime.strptime(
+                        row["START_DATE"][:10], '%Y-%m-%d'
+                    ).date()
+                else:
+                    model_args["start_date"] = datetime.datetime.strptime(
+                        row["START_DATE"][:10], '%d/%m/%Y'
+                    ).date()
 
-                episode = patient.episode_set.get()
+                model_args['trial_deleted'] = bool(int(model_args['trial_deleted']))
 
-                day = PeakFlowDay(episode=episode)
+                if OccTrial.objects.filter(
+                    occmedno=model_args["occmedno"],
+                    trial_num=model_args["trial_num"],
+                    trial_id=model_args["trial_id"],
+                    trial_deleted=model_args["trial_deleted"],
+                ).exists():
+                    if OccTrial.objects.filter(**model_args).exists():
+                        self.duplicate_trial_row += 1
+                        continue
+                    else:
+                        self.duplicate_trial_row_with_different_values += 1
+                        continue
+
+                OccTrial.objects.create(**model_args)
+
+    def import_occ_trial_day_file(self, occ_trial_day_file_name):
+        print(f'Import OCC Trial Day {occ_trial_day_file_name}')
+        OccTrialDay.objects.all().delete()
+        with open(occ_trial_day_file_name, mode='r', encoding='utf-8-sig') as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                model_args = {
+                    k.lower(): int(v) for k, v in row.items() if not k == "TRIAL_DATA"
+                }
+                model_args['published'] = bool(model_args['published'])
                 data = row["TRIAL_DATA"].split(',')[:-1]
 
                 flow_fields = [
@@ -124,19 +173,173 @@ class Command(BaseCommand):
                     'flow_1800', 'flow_1900', 'flow_2000',
                     'flow_2100', 'flow_2200', 'flow_2300',
                 ]
-
                 for i, field in enumerate(flow_fields):
-                    setattr(day, field, data[i])
+                    model_args[field] = int(float(data[i].rstrip('.')))
 
-                day.day_num    = int(row["DAY_NUM"])
-                day.trial_num  = int(row["TRIAL_NUM"])
-                day.work_start = int(row["WORK_START"])
-                day.work_end   = int(row["WORK_FINISH"])
+                if OccTrialDay.objects.filter(
+                    occmedno=model_args["occmedno"],
+                    trial_num=model_args["trial_num"],
+                    day_num=model_args["day_num"],
+                    published=model_args["published"],
+                ).exists():
+                    if OccTrialDay.objects.filter(**model_args).exists():
+                        self.duplicate_trial_day_row += 1
+                        continue
+                    else:
+                        v = OccTrialDay.objects.filter(
+                            occmedno=model_args["occmedno"],
+                            trial_num=model_args["trial_num"],
+                            day_num=model_args["day_num"],
+                            published=model_args["published"],
+                        ).get()
+                        self.duplicate_trial_day_row_with_different_values += 1
+                        continue
 
-                day.save()
-                self.flow_days_imported += 1
+                OccTrialDay.objects.create(**model_args)
 
-        print('Missed {}'.format(self.patients_missed))
-        print('Skipped {} (no hosp num)'.format(self.no_hosp_num))
-        print('Found {}'.format(PeakFlowIdentifier.objects.all().count()))
-        print('With {} peak flow days'.format(self.flow_days_imported))
+    def import_data(self, dir_name):
+        demographics_file_name = os.path.join(dir_name, "OCC_PATIENT.CSV")
+        trial_day_file_name = os.path.join(dir_name, "OCC_TRIAL_DAY.CSV")
+        trial_file_name = os.path.join(dir_name, "OCC_TRIAL.CSV")
+
+        if not os.path.exists(demographics_file_name):
+            raise ValueError(
+                f"We expect a demographics file called {demographics_file_name}"
+            )
+
+        if not os.path.exists(trial_day_file_name):
+            raise ValueError(
+                f"We expect a demographics file called {trial_day_file_name}"
+            )
+
+        if not os.path.exists(trial_file_name):
+            raise ValueError(
+                f"We expect a demographics file called {trial_file_name}"
+            )
+
+        # demographics checks
+        self.patients_missed = 0
+        self.no_hosp_num = 0
+
+        # trial checks
+        self.no_start_date = 0
+        self.duplicate_trial_row = 0
+        self.duplicate_trial_row_with_different_values = 0
+
+        # trial day checks
+        self.duplicate_trial_day_row = 0
+        self.duplicate_trial_day_row_with_different_values = 0
+
+        self.import_demographics_file(demographics_file_name)
+        self.import_occ_trial_file(trial_file_name)
+        self.import_occ_trial_day_file(trial_day_file_name)
+
+        print("=== Demographics ===")
+        print(f'Missed {self.patients_missed}')
+        print(f'No hospital number {self.no_hosp_num}')
+        print(f'Total skipped {self.patients_missed + self.no_hosp_num}')
+        print(f'Identifiers added {PeakFlowIdentifier.objects.all().count()}')
+
+        occ_trials = OccTrial.objects.filter(trial_deleted=False).exclude(
+            patient=None
+        )
+        occ_trial_days = OccTrialDay.objects.filter(published=True).exclude(
+            patient=None
+        )
+
+        print("=== Trials ===")
+        print(f"No start date {self.no_start_date}")
+        print(f"Duplicate trial rows {self.duplicate_trial_row}")
+        print(f"Duplicate trial rows with different values {self.duplicate_trial_row_with_different_values}")
+        print(f"Total skipped {self.no_start_date + self.duplicate_trial_row_with_different_values}")
+        print(f"Trials added {occ_trials.count()}")
+
+        print("=== Trial days ===")
+        print(f"Duplicate rows with the same values {self.duplicate_trial_day_row}")
+        print(f"Duplicate rows with the different values {self.duplicate_trial_day_row_with_different_values}")
+        print(f"Total skipped {self.duplicate_trial_day_row_with_different_values}")
+        print(f"Total added {occ_trial_days.count()}")
+
+    def process_trial_days(self):
+        created = 0
+        patients = Patient.objects.exclude(peakflowidentifier=None)
+        patients = patients.exclude(episode__peakflowday=None)
+
+        # we know this patient has a peak flow entry and will handle manually
+        patients = patients.exclude(id=32333)
+
+        if patients:
+            print(patients.get().demographics().name)
+            raise ValueError(
+                'We already have peak flow days loaded for these patients'
+            )
+
+        # for the patient that has a new peak flow, we'll bump their trial num
+        # by one
+        to_update = PeakFlowDay.objects.filter(episode__patient_id=32333)
+        for i in to_update:
+            i.trial_num += 1
+            i.save()
+
+        occ_trials = OccTrial.objects.filter(trial_deleted=False).exclude(
+            patient=None
+        )
+        occ_trial_days = OccTrialDay.objects.exclude(
+            patient=None
+        )
+
+        for trial in occ_trials:
+            days = occ_trial_days.filter(
+                patient=trial.patient, trial_num=trial.trial_num
+            )
+            for day in days:
+                model_args = {
+                    "episode": trial.patient.episode_set.get(),
+                    "date": trial.start_date + datetime.timedelta(day.day_num),
+                    "treatment_taken": day.drug,
+                    "work_day": day.is_work_day
+                }
+                fields = [
+                    "flow_0000",
+                    "flow_0100",
+                    "flow_0200",
+                    "flow_0300",
+                    "flow_0400",
+                    "flow_0500",
+                    "flow_0600",
+                    "flow_0700",
+                    "flow_0800",
+                    "flow_0900",
+                    "flow_1000",
+                    "flow_1100",
+                    "flow_1200",
+                    "flow_1300",
+                    "flow_1400",
+                    "flow_1500",
+                    "flow_1600",
+                    "flow_1700",
+                    "flow_1800",
+                    "flow_1900",
+                    "flow_2000",
+                    "flow_2100",
+                    "flow_2200",
+                    "flow_2300",
+                    "trial_num",
+                    "day_num"
+                ]
+                for field in fields:
+                    model_args[field] = getattr(day, field)
+
+                PeakFlowDay.objects.get_or_create(
+                    **model_args
+                )
+
+                created += 1
+
+        print(f"Added {created}")
+
+    @transaction.atomic
+    def handle(self, *args, **kwargs):
+        dir_name = kwargs.get("directory_name")
+        self.import_data(dir_name)
+        self.process_trial_days()
--- legacy/models.py
+++ legacy/models.py
@@ -2,7 +2,7 @@
 Models for the RBH legacy transition
 """
 from django.db import models
-from opal.models import PatientSubrecord, EpisodeSubrecord
+from opal.models import PatientSubrecord, EpisodeSubrecord, Patient
 
 
 """
@@ -14,7 +14,7 @@ class ActionLog(EpisodeSubrecord):
     class Meta:
         verbose_name = 'General notes'
     # Now in rbhl.models.Employment
-    # employer       = models.CharField(blank=True, null=True, max_length=100)
+    # employer       = models.CharField( max_length=100)
     # oh_provider    = models.CharField(blank=True, null=True, max_length=100)
 
     # Now in rbhl.models.ClinicLog
@@ -164,4 +164,119 @@ Peak Flow DB
 
 
 class PeakFlowIdentifier(PatientSubrecord):
-    occmendo = models.IntegerField(blank=True, null=True)
+    MALE = "Male"
+    FEMALE = "Female"
+
+    SEX = (
+        (MALE, MALE,), (FEMALE, FEMALE,),
+    )
+    patient = models.ForeignKey(
+        Patient, blank=True, null=True, on_delete=models.CASCADE
+    )
+    occmedno = models.IntegerField(unique=True, blank=True, null=True)
+    height = models.IntegerField()
+    sex = models.CharField(max_length=256, choices=SEX)
+    name = models.CharField(max_length=256)
+    company = models.CharField(max_length=256)
+    hospital_number = models.CharField(max_length=256, blank=True, null=True)
+
+    def trials(self):
+        return OccTrial.objects.filter(occmedno=self.occmedno)
+
+    def trial_days(self):
+        return OccTrialDay.objects.filter(occmedno=self.occmedno)
+
+
+class OccTrialDay(models.Model):
+    DRUG_MAPPING = {
+        0: None,
+        1: "Only",
+        2: "Beta",
+        3: "Oral Steroid",
+        4: "Other"
+    }
+
+    patient = models.ForeignKey(
+        Patient, blank=True, null=True, on_delete=models.CASCADE
+    )
+    occmedno = models.IntegerField()
+    trial_num = models.IntegerField()
+    drug_code = models.IntegerField()
+    work_start = models.IntegerField()
+    work_finish = models.IntegerField()
+    day_num = models.IntegerField()
+    published = models.BooleanField()
+    flow_0000 = models.IntegerField(verbose_name="00:00")
+    flow_0100 = models.IntegerField(verbose_name="01:00")
+    flow_0200 = models.IntegerField(verbose_name="02:00")
+    flow_0300 = models.IntegerField(verbose_name="03:00")
+    flow_0400 = models.IntegerField(verbose_name="04:00")
+    flow_0500 = models.IntegerField(verbose_name="05:00")
+    flow_0600 = models.IntegerField(verbose_name="06:00")
+    flow_0700 = models.IntegerField(verbose_name="07:00")
+    flow_0800 = models.IntegerField(verbose_name="08:00")
+    flow_0900 = models.IntegerField(verbose_name="09:00")
+    flow_1000 = models.IntegerField(verbose_name="10:00")
+    flow_1100 = models.IntegerField(verbose_name="11:00")
+    flow_1200 = models.IntegerField(verbose_name="12:00")
+    flow_1300 = models.IntegerField(verbose_name="13:00")
+    flow_1400 = models.IntegerField(verbose_name="14:00")
+    flow_1500 = models.IntegerField(verbose_name="15:00")
+    flow_1600 = models.IntegerField(verbose_name="16:00")
+    flow_1700 = models.IntegerField(verbose_name="17:00")
+    flow_1800 = models.IntegerField(verbose_name="18:00")
+    flow_1900 = models.IntegerField(verbose_name="19:00")
+    flow_2000 = models.IntegerField(verbose_name="20:00")
+    flow_2100 = models.IntegerField(verbose_name="21:00")
+    flow_2200 = models.IntegerField(verbose_name="22:00")
+    flow_2300 = models.IntegerField(verbose_name="23:00")
+
+    class Meta:
+        unique_together = (
+            ('trial_num', 'occmedno', 'day_num', 'published',),
+        )
+
+    @property
+    def drug(self):
+        return self.DRUG_MAPPING.get(self.drug_code)
+
+    @property
+    def is_work_day(self):
+        if self.work_start and self.work_finish:
+            return True
+        return False
+
+    def save(self, *args, **kwargs):
+        if not self.patient_id:
+            pfi = PeakFlowIdentifier.objects.filter(
+                occmedno=self.occmedno
+            ).first()
+            if pfi:
+                self.patient = pfi.patient
+        super().save(*args, **kwargs)
+
+
+class OccTrial(models.Model):
+    patient = models.ForeignKey(
+        Patient, blank=True, null=True, on_delete=models.CASCADE
+    )
+    trial_id = models.IntegerField()
+    occmedno = models.IntegerField()
+    trial_num = models.IntegerField()
+    trial_days = models.IntegerField()
+    start_date = models.DateField()
+    trial_deleted = models.BooleanField()
+
+    class Meta:
+        unique_together = (
+            ('trial_id', 'occmedno', 'trial_num', 'trial_deleted',),
+        )
+
+    def save(self, *args, **kwargs):
+        if not self.patient_id:
+            pfi = PeakFlowIdentifier.objects.filter(
+                occmedno=self.occmedno
+            ).first()
+            if pfi:
+                self.patient = pfi.patient
+        super().save(*args, **kwargs)
