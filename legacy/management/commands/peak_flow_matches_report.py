06889d0c99ae04970e38a6672ff4a8508c51f01c
fredkingham <fredkingham@gmail.com>
2020-06-02 14:56:46 +0100

    fixes the flake 8 issues

--- legacy/management/commands/peak_flow_matches_report.py
+++ legacy/management/commands/peak_flow_matches_report.py
@@ -4,14 +4,10 @@ when importing historic peak flow data.
 """
 import os
 import csv
-import sys
 
 from django.core.management.base import BaseCommand
 from plugins.trade import match
 from plugins.trade.exceptions import PatientNotFoundError
-
-from rbhl.models import PeakFlowDay
-from legacy.models import PeakFlowIdentifier
 
 
 class Matcher(match.Matcher):
@@ -29,7 +25,7 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
 
         dir_name = kwargs.get("directory_name")
-        demographics_file_name = os.path.join(dir_name, "OCC_PATIENT.csv" )
+        demographics_file_name = os.path.join(dir_name, "OCC_PATIENT.csv")
 
         self.patients_matched = 0
         self.patients_missed = 0
@@ -37,7 +33,7 @@ class Command(BaseCommand):
 
         self.missed_dicts = []
 
-#        print('Match Peak Flow IDs')
+        print('Match Peak Flow IDs')
         with open(demographics_file_name) as f:
             reader = csv.DictReader(f)
             for row in reader:
@@ -48,24 +44,12 @@ class Command(BaseCommand):
 
                 matcher = Matcher(row)
                 try:
-                    patient = matcher.direct_match()
+                    matcher.direct_match()
                     self.patients_matched += 1
                 except PatientNotFoundError:
                     self.patients_missed += 1
                     self.missed_dicts.append(row)
                     continue
 
-        numbes_below_500 = 0
         for row in self.missed_dicts:
             print(",".join(row.values()))
-
-            #if int(row['\ufeffOCCMEDNO']) <= 500:
-            #     numbes_below_500 += 1
-
-            # else:
-            #     print('{} {}'.format(row['CRN'], row['PAT_NAME']))
-
-        # print('Matched {}'.format(self.patients_matched))
-        # print('Missed {}'.format(self.patients_missed))
-        # print('Below 500 {}'.format(numbes_below_500))
-        # print('Skipped {} (no hosp num)'.format(self.no_hosp_num))
