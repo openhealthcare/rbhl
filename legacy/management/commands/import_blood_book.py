c49efe147ceb95280c8047345c9adc802bb3ed7c
fredkingham <fredkingham@gmail.com>
2020-06-26 15:37:38 +0100

    This changes it so that the management takes a file.
    The matcher was then blowing up because it does a get with a
    demographics dict and we have duplicate patients.
    
    These patients are now excluded.

--- legacy/management/commands/import_blood_book.py
+++ legacy/management/commands/import_blood_book.py
@@ -1,52 +1,37 @@
 """
 Management command to import the blood book csv
 """
+import csv
 from django.core.management import BaseCommand
-import ffs
-
-from opal.core import match
+from django.db import transaction
 
 from legacy import episode_categories
 from legacy.utils import str_to_date
-from legacy.models import Referral, BloodBook, BloodBookResult
-
-
-class Matcher(match.Matcher):
-    direct_match_field = match.Mapping('hosp_no', 'hospital_number')
-    attribute_match_fields = [
-        match.Mapping('firstname', 'first_name'),
-        'surname',
-        match.Mapping('birth', 'date_of_birth')
-    ]
-    demographics_fields = [
-        match.Mapping('firstname', 'first_name'),
-        'surname',
-        match.Mapping('birth', 'date_of_birth'),
-        match.Mapping('hosp_no', 'hospital_number')
-    ]
+from legacy.models import BloodBook, BloodBookResult
+from opal.models import Patient
+from rbhl.models import Demographics
 
 
 def create_blood_book(row, episode):
-    print('Creating blood book entry')
     book = BloodBook(episode=episode)
-    book.reference_number = row.reference_no
-    book.blood_date = str_to_date(row.blooddat)
-    book.blood_number = row.bloodno
-    book.method = row.method
-    book.blood_collected = row.edta_blood_collected
-    book.date_dna_extracted = row.date_dna_extracted
-    book.information = row.information
-    book.assayno = row.assayno
-    book.assay_date = str_to_date(row.assaydate)
-    book.blood_taken = str_to_date(row.bloodtk)
-    book.blood_tm = str_to_date(row.bloodtm)
-    book.report_dt = str_to_date(row.reportdt)
-    book.report_st = str_to_date(row.reportst)
-    book.employer = row.employer
-    book.store = row.store
-    book.exposure = row.exposure
+    book.reference_number = row['REFERENCE NO']
+    book.blood_date = str_to_date(row["BLOODDAT"])
+    book.blood_number = row["BLOODNO"]
+    book.method = row["METHOD"]
+    book.blood_collected = row['EDTA blood collected']
+    book.date_dna_extracted = row["Date DNA extracted"]
+    book.information = row["INFORMATION"]
+    book.assayno = row["ASSAYNO"]
+    book.assay_date = str_to_date(row["ASSAYDATE"])
+    book.blood_taken = str_to_date(row["BLOODTK"])
+    book.blood_tm = str_to_date(row["BLOODTM"])
+    book.report_dt = str_to_date(row["REPORTDT"])
+    book.report_st = str_to_date(row["REPORTST"])
+    book.employer = row["Employer"]
+    book.store = row["STORE"]
+    book.exposure = row["EXPOSURE"]
     try:
-        book.antigen_date = str_to_date(row.antigendat)
+        book.antigen_date = str_to_date(row["ANTIGENDAT"])
     except ValueError:
         # We know that sometimes the data claims that the value of this field
         # should be month number -1098.
@@ -54,59 +39,107 @@ def create_blood_book(row, episode):
         # Strptime will complain that -1098 is not a month in the format %m.
         # This seems eminently reasonable. Allow it to complain, but move on.
         pass
-    book.antigen_type = row.antigentyp
-    book.comment = row.comment
-    book.oh_provider = row.oh_provider
-    book.batches = row.batches
-    book.room = row.room
-    book.freezer = row.freezer
-    book.shelf = row.shelf
-    book.tray = row.tray
-    book.vials = row.vials
-    book.save()
+    book.antigen_type = row["ANTIGENTYP"]
+    book.comment = row["Comment"]
+    book.oh_provider = row["OH Provider"]
+    book.batches = row["Batches"]
+    book.room = row["Room"]
+    book.freezer = row["Freezer"]
+    book.shelf = row["Shelf"]
+    book.tray = row["Tray"]
+    book.vials = row["Vials"]
+    return book
+
+
+def get_demographics(**demographics_kwargs):
+    demographics = Demographics.objects.filter(
+        **demographics_kwargs
+    )
+    if len(demographics) == 1:
+        return demographics[0]
+    elif len(demographics) > 1:
+        msg = "Duplicate patient for {}".format(
+            demographics_kwargs
+        )
+        print(msg)
+        raise ValueError(msg)
+    return None
 
 
 class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument("file_name", help="Specify import file")
+
+    @transaction.atomic
     def handle(self, *args, **options):
-        data = ffs.Path('~/Documents/RBHL/bloods/bloodbook.csv')
-        self.patients_imported = 0
+        patients_imported = 0
 
         print('Open CSV to read')
-        with data.csv(header=True) as csv:
+        with open(options["file_name"]) as f:
+            reader = csv.DictReader(f)
+            duplicates = []
+            referrals = []
+            books = []
+            results = []
 
-            for row in csv:
-                print('Matching patient')
-
-                data = row._asdict()
-
-                data['birth'] = str_to_date(
-                    data['birth'], no_future_dates=True
+            for data in reader:
+                hospital_number = data["Hosp_no"].strip()
+                demographics = None
+                dob = str_to_date(
+                    data['BIRTH'], no_future_dates=True
                 )
 
-                matcher = Matcher(data)
-                # print(matcher.get_demographic_dict())
-                # import sys;sys.exit()
-                patient, created = matcher.match_or_create()
+                if hospital_number:
+                    try:
+                        demographics = get_demographics(
+                            hospital_number=data["Hosp_no"].strip()
+                        )
+                        if demographics and not demographics.date_of_birth:
+                            demographics.date_of_birth = dob
+                            demographics.save()
+                    except ValueError:
+                        # If we have a patient with a duplicate
+                        # hospital number try a lookup with name/dob
+                        pass
 
-                if not created:
-                    demographics = patient.demographics_set.get()
-                    demographics.date_of_birth = data['birth']
-                    demographics.save()
+                if not demographics:
+                    try:
+                        demographics = get_demographics(
+                            first_name__iexact=data["FIRSTNAME"].strip(),
+                            surname__iexact=data["SURNAME"].strip(),
+                            date_of_birth=dob
+                        )
+                    except ValueError:
+                        duplicates.append(data)
+                        continue
 
-                print('Creating episdoe')
+                if demographics:
+                    patient = demographics.patient
+                else:
+                    patient = Patient.objects.create()
+                    patient.demographics_set.update(
+                        hospital_number=data["Hosp_no"],
+                        first_name=data["FIRSTNAME"],
+                        surname=data["SURNAME"],
+                        date_of_birth=dob
+                    )
+                    patients_imported += 1
+
+                data['BIRTH'] = str_to_date(
+                    data['BIRTH'], no_future_dates=True
+                )
+
                 episode = patient.create_episode(
                     category_name=episode_categories.BloodBook.display_name
                 )
 
-                print('Creating referral')
-                referral = Referral(episode=episode)
-                referral.referrer_name = row.referrername
-                referral.referrer_title = row.referrerttl
+                referral = episode.referral_set.get()
+                referral.referrer_name = data["Referrername"]
+                referral.referrer_title = data["Referrerttl"]
                 referral.save()
+                referrals.append(referral)
 
-                create_blood_book(row, episode)
-
-                print('Creating Blood results')
+                books.append(create_blood_book(data, episode))
 
                 fieldnames = [
                     'result', 'allergen', 'antigenno', 'kul',
@@ -117,7 +150,7 @@ class Command(BaseCommand):
                     result_data = {}
                     for field in fieldnames:
                         iterfield = '{}{}'.format(field, str(i))
-                        value = getattr(row, iterfield, "")
+                        value = data.get(iterfield, "")
                         if value:
                             if field == 'class':
                                 field = 'klass'
@@ -126,9 +159,25 @@ class Command(BaseCommand):
                     if any(result_data.values()):
                         result_data['episode'] = episode
                         result = BloodBookResult(**result_data)
-                        result.save()
-                self.patients_imported += 1
-                print('{} ({})'.format(
-                    patient.demographics_set.get().name,
-                    self.patients_imported)
-                )
+                        results.append(result)
+
+        # although they are actually updated they singletons
+        # therefore essentially created
+        msg = "Created {} referrals".format(len(referrals))
+        self.stdout.write(self.style.SUCCESS(msg))
+
+        BloodBook.objects.bulk_create(books)
+        msg = "Created {} blood books".format(len(books))
+        self.stdout.write(self.style.SUCCESS(msg))
+
+        BloodBookResult.objects.bulk_create(results)
+        msg = "Created {} blood results".format(len(results))
+        self.stdout.write(self.style.SUCCESS(msg))
+
+        msg = "Skipping {} duplicates".format(len(duplicates))
+        self.stdout.write(self.style.WARNING(msg))
+
+        msg = "Impored {} patients".format(
+            patients_imported
+        )
+        self.stdout.write(self.style.SUCCESS(msg))
