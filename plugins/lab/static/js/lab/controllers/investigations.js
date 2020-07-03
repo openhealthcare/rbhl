aefaa2bfba4a90aa4ab5a04bc03882167847f675
fredkingham <fredkingham@gmail.com>
2020-07-03 16:59:07 +0100

    Changes it so that a lab test is all displayed on 1 line in the format that the result was written

--- plugins/lab/static/js/lab/controllers/investigations.js
+++ plugins/lab/static/js/lab/controllers/investigations.js
@@ -1,6 +1,16 @@
 angular.module('opal.controllers').controller('InvestigationsView', function($scope, displayDateFilter, SkinPrickTestHelper) {
   "use strict";
 
+  // Bloodbook obervation names
+  var RESULT = "Result"
+  var ALLERGEN = "Allergen"
+  var ANTIGEN_NO = "Antigen #"
+  var KUL = "KU/L"
+  var CLASS = "Class"
+  var RAST = "RAST %B"
+  var PRECIPITIN = "Precipitin"
+  var IGG = "mg/L"
+
   this.getTestDateKeys = function(){
     /*
     * Returns a list of strings of dates ordered in descending order
@@ -50,8 +60,67 @@ angular.module('opal.controllers').controller('InvestigationsView', function($sc
     return SkinPrickTestHelper.isAtopic(skinPrickTests);
   }
 
+  this.labTestObsToObj = function(observationArray){
+    var result = {};
+    _.each(observationArray, function(obs){
+      result[obs.observation_name] = obs.observation_value
+    });
+    return result;
+  }
+
+  this.getTestDisplay = function(observationArray){
+    var labTestObj = this.labTestObsToObj(observationArray);
+    var label = "";
+    var value = [];
+
+    if(labTestObj[ALLERGEN] && labTestObj[ALLERGEN].length){
+      label = labTestObj[ALLERGEN];
+
+      if(labTestObj[ANTIGEN_NO]){
+        label = label + " (" + labTestObj[ANTIGEN_NO] + ")";
+      }
+
+      if(labTestObj[KUL]){
+        value.push(labTestObj[KUL] + " kU/L");
+      }
+
+      if(labTestObj[CLASS]){
+        value.push(labTestObj[CLASS] + " CLASS");
+      }
+
+      if(labTestObj[RAST]){
+        var rast = labTestObj[RAST].substring(0, 5);
+        rast = Math.round(parseFloat(rast), 2);
+        value.push(rast + " %B");
+      }
+
+      if(labTestObj[PRECIPITIN]){
+        value.push(labTestObj[PRECIPITIN] + " mg/L");
+      }
+    }
+    else if(labTestObj[RESULT] && labTestObj[RESULT].trim().length){
+      var splitted = labTestObj[RESULT].trim().split(")");
+      if(splitted.length > 1){
+        label = splitted.shift() + ")"
+        value.push(splitted.join(")"));
+      }
+      else{
+        var splitted = labTestObj[RESULT].trim().split(" ", 1);
+        if(splitted.length === 2){
+          label = splitted.shift();
+          value.push(splitted).join(" ");
+        }
+      }
+    }
+    return {
+      label: label,
+      value: value
+    }
+  }
+
   this.getAntigenNo = function(array){
-    var result = _.findWhere(array, {observation_name: "Antigen #"})
+    var title = "";
+    var allergenNo = _.findWhere(array, {observation_name: "Antigen #"})
     if(result){
       return result.observation_value;
     }
