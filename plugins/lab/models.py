0eb05b9841fad2711dfa514b41197f317976685a
fredkingham <fredkingham@gmail.com>
2020-09-24 21:54:49 +0100

    merges in the plugin models from blood-book-patient-episode-creation

--- plugins/lab/models.py
+++ plugins/lab/models.py
@@ -114,3 +114,94 @@ class OtherInvestigations(RbhlSubrecord, models.PatientSubrecord):
         choices=TEST_TYPE, blank=True, default="", max_length=256
     )
     details = fields.TextField(blank=True, default="")
+
+
+class Allergen(lookuplists.LookupList):
+    pass
+
+
+class Exposure(lookuplists.LookupList):
+    pass
+
+
+class BloodBook(RbhlSubrecord, models.PatientSubrecord):
+    ANTIGEN_TYPE = enum("STANDARD", "BESPOKE")
+    METHODS = enum(
+        "ImmunoCAP",
+        "UniCAP",
+        "CAP",
+        "RAST",
+        "CAP & RAST",
+        "RAST & UniCAP",
+        "PRECIPITINS",
+    )
+
+    # we're storing this because it comes in, but won't display it
+    # on the front end
+    reference_number = fields.CharField(blank=True, null=True, max_length=256)
+
+    # Blood details
+
+    # This is called Blooddat
+    sample_received = fields.DateField(blank=True, null=True)
+    # an old id of the blood result.
+    blood_number = fields.CharField(blank=True, null=True, max_length=200)
+    blood_taken = fields.DateTimeField(blank=True, null=True)
+    store = fields.NullBooleanField(blank=True)
+
+    # antigen test details
+    assay_number = fields.CharField(blank=True, null=True, max_length=200)
+    assay_date = fields.DateField(blank=True, null=True)
+    method = fields.CharField(blank=True, null=True, max_length=256, choices=METHODS)
+    antigen_type = fields.CharField(
+        blank=True, null=True, max_length=256, choices=ANTIGEN_TYPE
+    )
+    antigen_date = fields.DateField(blank=True, null=True)
+
+    # report information these fields don't appear on the
+    # front end so first version won't display them.
+
+    # this is the field in the csv called reportdt
+    report_date = fields.DateField(blank=True, null=True)
+
+    # this is the field in the csv called reportst
+    report_submitted = fields.DateField(blank=True, null=True)
+
+    # A free text box for additional information
+    information = fields.TextField(blank=True, default="")
+
+
+class BloodBookResult(fields.Model):
+    PRECIPITIN_CHOICES = enum("-ve", "+ve", "Weak +ve", '++ve')
+
+    blood_book = fields.ForeignKey(BloodBook, on_delete=fields.CASCADE)
+    result = fields.TextField(blank=True, null=True, max_length=200)
+    allergen = models.ForeignKeyOrFreeText(Allergen)
+    antigen_number = fields.CharField(blank=True, null=True, max_length=200)
+
+    # usually a float but can include '<' and '>'
+    kul = fields.CharField(blank=True, null=True, max_length=200, verbose_name="KU/L")
+    ige_class = fields.CharField(
+        blank=True,
+        null=True,
+        max_length=200,
+        verbose_name="IgE Class",
+        choices=enum(*[str(i) for i in range(7)]),
+    )
+    rast = fields.FloatField(
+        blank=True, null=True, verbose_name="RAST %B"
+    )
+    precipitin = fields.CharField(
+        blank=True,
+        null=True,
+        max_length=200,
+        verbose_name="Precipitin",
+        choices=PRECIPITIN_CHOICES,
+    )
+    igg_mg = fields.FloatField(
+        blank=True, null=True, verbose_name="mg/L"
+    )
+    # This has been filled in twice in the entire history, not sure if we need it.
+    igg_class = fields.IntegerField(
+        blank=True, null=True, verbose_name="IgG class"
+    )
