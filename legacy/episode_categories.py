7b222af11fb003471382e8652dd6e52fec34c64b
fredkingham <fredkingham@gmail.com>
2020-09-24 21:52:45 +0100

    remove the legacy blood book episode category

--- legacy/episode_categories.py
+++ legacy/episode_categories.py
@@ -4,8 +4,3 @@ from opal.core import episodes
 class AdminDatabase(episodes.EpisodeCategory):
     display_name = 'Admin Database'
     detail_template = 'detail/admin_database.html'
-
-
-class BloodBook(episodes.EpisodeCategory):
-    display_name    = 'Blood Book'
-    detail_template = 'detail/blood_book.html'
