b907aec70b820fe30a5a1a22d85089105b5e0ae8
fredkingham <fredkingham@gmail.com>
2020-09-25 10:07:10 +0100

    Display the investigations count in the switcher

--- rbhl/static/js/rbhl/filters.js
+++ rbhl/static/js/rbhl/filters.js
@@ -10,7 +10,7 @@ filters.filter('testCount', function(displayDateFilter){
     * be a different test type
     */
    var count = 0;
-   _.each(["spirometry", "bronchial_test", "skin_prick_test"], function(testType){
+   _.each(["spirometry", "bronchial_test", "skin_prick_test", 'blood_book'], function(testType){
      // date equality in js is awkward, casting it to a date handles nulls and gives us date
      // rather than datetime equality
      var displayDates = _.map(patient[testType], function(someTest){ return displayDateFilter(someTest.date)})
