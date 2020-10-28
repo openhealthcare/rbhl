4ddc77e3e0a1e876a5f8663efef8cc02f7955c09
fredkingham <fred.kingham@openhealthcare.org.uk>
2020-10-28 13:35:08 +0000

    Adds a delete option to the blood test pathway. This has a confirmation modal using opal's core delete item logic.

--- legacy/static/js/legacy/controllers/blood_book.js
+++ legacy/static/js/legacy/controllers/blood_book.js
@@ -1,5 +1,5 @@
 angular.module('opal.controllers').controller(
-  'BloodBookStep', function(scope, step, episode, $location) {
+  'BloodBookStep', function(scope, step, episode, $window, $location, $modal) {
   /*
   * The blood book step works on a single blood book instance.
   * if there is an id as a GET param in the url, then it is that instance
@@ -36,6 +36,22 @@ angular.module('opal.controllers').controller(
 
   scope.removeResult = function(idx){
     scope.bloodBookTest.blood_book.bloodbookresult_set.splice(idx, 1);
+  }
+
+  scope.delete = function(){
+    var item = _.findWhere(episode.blood_book, {id: scope.bloodBookTest.blood_book.id})
+    var deleteModal =  $modal.open({
+      templateUrl: '/templates/pathway/delete_blood_book_modal.html',
+      controller: 'DeleteItemConfirmationCtrl',
+      resolve: {
+        item: item
+      }
+    });
+    deleteModal.result.then(function(result){
+      if(result === 'deleted'){
+        $window.location.href = "/#/patient/" + item.patient_id + "/investigations";
+      }
+    });
   }
 
   scope.preSave = function(editing){
