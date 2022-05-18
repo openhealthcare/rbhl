ac295533cb42eded74cfb06e190c395eb04eb233
Fred <fredkingham@users.noreply.github.com>
2022-05-18 20:48:07 +0100

    Revert "Changes new referral to new patient"

--- rbhl/__init__.py
+++ rbhl/__init__.py
@@ -51,7 +51,7 @@ class Application(application.OpalApplication):
     @classmethod
     def get_menu_items(klass, user=None):
         # we import here as settings must be set before this is imported
-        from rbhl.pathways import NewPatient, NewLabPatient
+        from rbhl.pathways import NewReferral, LabReferral
 
         if user:
             if user.is_authenticated:
@@ -61,9 +61,9 @@ class Application(application.OpalApplication):
 
                 if lab_user:
                     items = [
-                        NewLabPatient.as_menuitem(
+                        LabReferral.as_menuitem(
                             index=1,
-                            display="New patient"
+                            display="New referral"
                         ),
                         menus.MenuItem(
                             activepattern=reverse('unresulted-list'),
@@ -81,7 +81,7 @@ class Application(application.OpalApplication):
                         )
                     ]
                 else:
-                    items = [NewPatient.as_menuitem(index=1)]
+                    items = [NewReferral.as_menuitem(index=1)]
 
                 items.append(
                     menus.MenuItem(
