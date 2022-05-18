b955cd27c284009cba40ae5ceeaf213987e755c2
fredkingham <fred.kingham@openhealthcare.org.uk>
2022-05-18 14:22:09 +0100

    Add a full list of everything that will be deleted when you delete a referral and add a sum check before they can delete it

--- rbhl/static/js/rbhl/controllers/delete_episode.js
+++ rbhl/static/js/rbhl/controllers/delete_episode.js
@@ -4,7 +4,56 @@ angular
     $scope, $http, $window, $q, episode, $modalInstance, callBack
   ) {
     "use strict";
-		$scope.episode = episode;
+		function init(){
+			$scope.episode = episode;
+			$scope.is_populated = $scope.are_there_entries();
+			$scope.peak_flow_day_count = $scope.get_peak_flow_day_count();
+		}
+
+		function getRandomInt(max) {
+			return Math.floor(Math.random() * max);
+		}
+
+		$scope.mathCheck = {
+			int1: getRandomInt(10),
+			int2: getRandomInt(10),
+			result: ""
+		}
+
+		$scope.answered = function(){
+			var result = parseInt($scope.mathCheck.result);
+			if(result && $scope.mathCheck.int1 + $scope.mathCheck.int2 === result){
+				return true;
+			}
+		}
+
+		$scope.get_peak_flow_day_count = function(){
+			return _.uniq(_.pluck($scope.episode.peak_flow_day, "trial_num")).length
+		}
+
+		$scope.are_there_entries = function(){
+			if(episode.employment[0].consistency_token){
+				return true;
+			}
+			if(episode.social_history[0].consistency_token){
+				return true;
+			}
+			if(episode.diagnosis.length){
+				return true;
+			}
+			if(episode.action_log.length){
+				return true;
+			}
+			if(episode.letter.length){
+				return true
+			}
+			if(episode.peak_flow_day.length){
+				return true
+			}
+			if(episode.clinic_log[0].consistency_token){
+				return true;
+			}
+		}
 
 		var url = '/indigo/v0.1/occld_episode/';
 		$scope.delete = function(){
@@ -22,4 +71,6 @@ angular
 		$scope.cancel = function() {
 			$modalInstance.close('cancel');
 		};
+
+		init();
 	});
