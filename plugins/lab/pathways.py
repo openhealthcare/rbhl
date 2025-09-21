e0e6fcc24184eece76de75b8e55ef19e80ff39b3
David Miller <david@openhealthcare.org.uk>
2025-09-21 18:42:34 +0100

    WIP CT CHest pathwway

--- plugins/lab/pathways.py
+++ plugins/lab/pathways.py
@@ -33,3 +33,27 @@ class Bloods(PagePathway):
 
     def redirect_url(self, user=None, patient=None, episode=None):
         return "/#/patient/{0}/investigations".format(patient.id)
+
+
+class CTChestPathway(PagePathway):
+    """
+    As it's bafflingly hard to do non-global form defaults, we create
+    this entire pathway to write the string 'CT chest scan' in the field `test`.
+
+    Better implementation suggestions definitely welcome.
+    """
+
+    icon         = 'fa-crosshairs'
+    display_name = 'CT Chest'
+    slug         = 'ct-chest'
+    template     = 'pathway/other_investigations_base.html'
+    steps        = [
+        Step(
+            models.OtherInvestigations,
+            base_template='pathway/ct_chest_form.html',
+            step_controller="CTChestPathwayStepController"
+        )
+    ]
+
+    def redirect_url(self, user=None, patient=None, episode=None):
+        return "/#/patient/{0}/investigations".format(patient.id)
